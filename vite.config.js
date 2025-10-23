7eb2f556116b0462148f79203f787170720ec75a
Allan Ratemo <allanisratemo@gmail.com>
2025-10-23 12:44:03 +0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Bot-Battlr-Code-Challenge/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
